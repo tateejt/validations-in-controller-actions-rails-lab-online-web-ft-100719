50db6ccab33da038d9d7b4204f4e87b5cc84376e
Emery Tate <tateejt@gmail.com>
2020-01-12 03:56:42 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,8 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create!(author_params)
 
-  if @author
+  if @author.valid?
+    
     redirect_to author_path(@author)
   end
 
