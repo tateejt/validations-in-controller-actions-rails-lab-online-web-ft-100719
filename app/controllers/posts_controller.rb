c462f01a6e51d6971dfec3c49cf37d34315d6fee
Emery Tate <tateejt@gmail.com>
2020-01-12 04:00:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
   validates :title, presences: true 
-   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :content, length: {minimum: 100}
 
   def show
