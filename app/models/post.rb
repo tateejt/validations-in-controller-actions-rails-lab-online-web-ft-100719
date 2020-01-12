866074b4ae64631c045640e5702bace12f584967
Emery Tate <tateejt@gmail.com>
2020-01-12 04:04:10 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validate :title, presences: true 
-  validate :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :content, length: {minimum: 100}
+  validates :title, presences: true 
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :content, length: {minimum: 100}
 end
